f815c47155de4b14ebab6ee375a8c3aa2ab55ad1
Danichok <danielreabyviktorovych@gmail.com>
2024-06-27 22:35:04 +0300

    upd

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -17,7 +17,7 @@ function Menu(props) {
     }
     
     return (
-        <div className='menu top-1 lg:top-[20px] right-1 lg:right-[20px] '>
+        <div className='menu top-1 lg:top-[30px] right-1 lg:right-[20px] '>
             <div className="menuSelect " onClick={e => setMenuOpen(!menuOpen)}>
                 <p className='selectedLanguageText clash'>{language}</p>
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
