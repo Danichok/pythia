2ea9afff1920047a8717a781cb7239ec29e868fd
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2024-07-08 17:19:15 +0300

    small text fixes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { useLanguage } from '../Helpers/LanguageContext'
 function Footer() {
     const { languageData } = useLanguage()
     return (
-        <div className=' w-full bg-[#545456] flex py-[20px] lg:py-[80px]'>
+        <div className=' w-full bg-[#545456] flex py-[20px] lg:py-[80px] lg:pb-[20px]'>
             <div className='max-w-[1160px] m-auto w-full px-6 lg:px-2'>
                 <p className='text-white font-extrabold text-[8px] lg:text-[24px]'>© PYTHIA PLUS LTD</p>
                 <p className='text-[#CBCBCC] font-normal text-[8px] lg:text-[24px] tracking-[1.2px] leading-normal'>{languageData?.homeFooterAddress1} <br /> {languageData?.homeFooterAddress2}
@@ -12,9 +12,13 @@ function Footer() {
                 <p className='text-[#CBCBCC] font-normal text-[8px] lg:text-[24px] tracking-[1.2px] leading-normal py-[12px] lg:py-[48px]'>{languageData?.homeFooterSubtitle}</p>
                 <hr className='h-[0.2px] lg:h-[1px] w-full bg-white mb-[12px] lg:mb-[48px]' />
                 <div className='flex justify-between items-center'>
+                    {
+                        /*
                    <a className='text-[#CBCBCC] font-normal text-[8px] lg:text-[24px] tracking-[1.2px] hover:text-white duration-150' href="#">{languageData?.homeFooterSocialLink1}</a>
                    <a className='text-[#CBCBCC] font-normal text-[8px] lg:text-[24px] tracking-[1.2px] hover:text-white duration-150' href="#">{languageData?.homeFooterSocialLink2}</a>
                    <a className='text-[#CBCBCC] font-normal text-[8px] lg:text-[24px] tracking-[1.2px] hover:text-white duration-150' href="#">{languageData?.homeFooterSocialLink3}</a>
+                        */
+                    }
                 </div>
             </div>
         </div>
